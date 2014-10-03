2d3ab6e54c504c0875027b5fc98271540db7b955
matthewgibson <mgibson5@mail.greenriver.edu>
2014-10-02 18:49:28 -0700

    Program01

--- cards.c
+++ cards.c
@@ -4,35 +4,58 @@
 int main()
 {
 	char card_name[3];
+
+	int count = 0;
+
+	while (card_name != 'X')
+	{
 	
-	puts("enter the card_name:");
+		puts("enter the card_name:");
 
-	scanf("%2s", card_name);
+		scanf("%2s", card_name);
 
-	int val = 0;
+		int val = 0;
 
-	if (card_name[0] == 'K') 
-	{
-		val = 10;
-	}
-	else if (card_name[0] == 'Q')
-	{
-		val = 10;
-	}
-	else if (card_name[0] == 'J')
-	{
-		val = 10;
-	}
-	else if (card_name[0] == 'A')
-	{
-		val = 11;
-	}
-	else 
-	{
-		val = atoi(card_name);
-	}
+		switch(card_name[0])
+		{
 
-	printf("The card value is: %i\n", val);
+		case 'K': 
+	
+		case 'Q':
+		
+		case 'J':
+		
+			val = 10;
+			break;
+
+		case 'A':
+		
+			val = 11;
+			break;
+
+		case 'X':
+			break;
+
+		default:
+			val = atoi(card_name);
+
+			if (val < 1 | val > 10)
+			{
+				puts("I don't understand that value!");
+				continue;
+			}
+		if ((val > 2 ) && (val < 7))
+		{
+			count++;
+		}
+		else if (val == 10)
+		{
+			count--;
+		}	
+		printf("The card value is: %i\n", count);
+		}
+		
+	}
 
 	return 0;
 }
